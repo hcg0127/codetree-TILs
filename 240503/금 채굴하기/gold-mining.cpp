0d6740f8884014cf595dab047fd7e33d47d66292
hcg0127 <115148838+hcg0127@users.noreply.github.com>
2024-05-03 12:39:01 +0900

    Title: 금 채굴하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240503/금 채굴하기/gold-mining.cpp
+++ 240503/금 채굴하기/gold-mining.cpp
@@ -3,23 +3,24 @@
 #include <algorithm>
 using namespace std;
 
-int n,m,board[20][20],k=0,ans=0,dx[4]={1,0,-1,0},dy[4]={0,1,0,-1};
+int n,m,board[20][20],k=0,ans=0,dx[4]={0,1,0,-1},dy[4]={1,0,-1,0};
 
-int bfs(int a, int b, int d) {
+int dia(int a, int b, int d) {
     queue<pair<int,int>> q;
-    bool visit[20][20]={};
-    int coin = (board[a][b]==1 ? 1 : 0);
-    fill(&visit[0][0],&visit[20][0],false);
+    int visit[20][20];
+    fill(&visit[0][0],&visit[20][0],0);
+    int coin = board[a][b]==1 ? 1 : 0;
     q.push({a,b});
-    visit[a][b]=true;
-    while (d--) {
+    visit[a][b]=1;
+    while (!q.empty()) {
         auto [x,y] = q.front();
         q.pop();
+        if (visit[x][y]>d) continue;
         for (int i=0; i<4; i++) {
             int cx=x+dx[i],cy=y+dy[i];
-            if (cx<0 || cy<0 || cx>=n || cy>=n || visit[cx][cy]) continue;
+            if (cx<0 || cy<0 || cx>=n || cy>=n || visit[cx][cy]!=0) continue;
             q.push({cx,cy});
-            visit[cx][cy]=true;
+            visit[cx][cy]=visit[x][y]+1;
             if (board[cx][cy]==1) coin++;
         }
     }
@@ -29,12 +30,14 @@ int bfs(int a, int b, int d) {
 int main() {
     cin >> n >> m;
     for (int i=0; i<n; i++)
-        for (int j=0; j<n; j++) cin >> board[i][j];
+        for (int j=0; j<n; j++)
+            cin >> board[i][j];
+
     while (k<n) {
         int tmp;
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
-                tmp = bfs(i,j,k);
+                tmp = dia(i,j,k);
                 if (m*tmp >= k*k + (k+1)*(k+1))
                     ans = max(ans,tmp);
             }
